139ae14c207632a9bf3fa4d2b944e7453a4eb9e3
Shouryaban_Samanta <111694596+Shouryaban19@users.noreply.github.com>
2025-09-08 21:22:18 +0530

    Add files via upload

---
+++ ArrayList/Student.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Student {
+	int id;
+	String name;
+	String course;
+	Student(int id, String name, String course) {
+		this.id = id;
+		this.name = name;
+		this.course = course;
+	}
+	@Override
+	public String toString() {
+		return "ID: " + id + " | Name: " + name + " | Course: " + course;
+	}
+}
